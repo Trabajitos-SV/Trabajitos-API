bb2de42133de16e24d5c20646000d62091838942
Michael Narváez <michaelnarvaez@Michaels-MacBook-Air.local>
2023-06-14 21:23:09 -0600

    adding mongoose DBRUI and PORT to the project

--- Trabajitos-SV-API/app.js
+++ Trabajitos-SV-API/app.js
@@ -2,16 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose  = require('./config/mongoose');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+mongoose.connect();
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

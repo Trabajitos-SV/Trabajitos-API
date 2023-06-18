86525966782a74eef11351699e60662c62932f25
Michael Narváez <michaelnarvaez@Michaels-MacBook-Air.local>
2023-06-18 10:28:16 -0600

    adding trabajito controller into main Controller folder

---
+++ Trabajitos-SV-API/controllers/trabajito.controller.js
@@ -0,0 +1,14 @@
+const Trabajito = require("../models/trabajito.model");
+const debug = require("debug")("app:trabajito-controller");
+
+
+const controller = {};
+
+
+
+
+
+
+
+
+module.exports = controller;

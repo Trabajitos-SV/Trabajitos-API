3faeb9e27a4a921eb4d9e4f6901bbc38d28dce28
Michael Narváez <michaelnarvaez@Michaels-MacBook-Air.local>
2023-06-18 21:29:46 -0600

    adding endTrabajito controller

--- Trabajitos-SV-API/controllers/trabajito.controller.js
+++ Trabajitos-SV-API/controllers/trabajito.controller.js
@@ -34,6 +34,33 @@ controller.createTrabajito = async() => {
 }
 
 
+constroller.endTrabajito = async(req, res) => {
+    try {
+        const { id: trabajitoId, endNumber } = req.body; 
+        const { _id: userID } = req.user;
+
+        const trabajito = await Trabajito.findOne({ _id: trabajitoId, id_hirred: userID});
+
+        if(!trabajito) {
+            return res.status(404).json({ error: "Trabajito not found" })
+        }
+
+        trabajito.endNumber = endNumber;
+
+        const updatedTrabajito = await trabajito.save();
+        if(!updatedTrabajito) {
+            return res.status(409).json({ error: "Unexpected error updating trabajito" })
+        }
+
+        return res.status(201).json(updatedTrabajito);
+
+    } catch (error) {
+        debug({ error })
+        return res.status(500).json({ error: "Internal server error" })     
+    }
+}
+
+
 
 
 

1cd4365a3c4d4f545f7f86fc07a6fb0eee27b315
de-camp <00165221@uca.edu.sv>
2023-06-24 10:43:31 -0600

    feat: created municipality controller, changed review controller it might not be used

---
+++ Trabajitos-SV-API/controllers/municipality.controller.js
@@ -0,0 +1,26 @@
+const Municipality = require("../models/municipality.model");
+const debug = require("debug")("app:municipality-controller");
+
+const controller = {};
+
+controller.createMunicipality = async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        const newMunicipality = new Municipality({
+            name: name
+        });
+
+        const savedMunicipality = await newMunicipality.save();
+        if (!savedMunicipality) {
+            return res.status(409).json({ error: "Municipality cannot be saved"})
+        }
+
+        return res.status(201).json({ message: "Successfully saved municipality!"});
+    } catch (error) {
+        debug({ error });
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+module.exports = controller;

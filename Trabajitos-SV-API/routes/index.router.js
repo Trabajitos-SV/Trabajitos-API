1b57bc9f7c3c63a270170a62d5f0c67edc67b996
de-camp <00165221@uca.edu.sv>
2023-06-24 16:48:09 -0600

    feat: modified index to access all routes, also deleted users router

--- Trabajitos-SV-API/routes/index.router.js
+++ Trabajitos-SV-API/routes/index.router.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/*Importing routers*/
+const authRouter = require("./api/auth.router");
+const trabajitoRouter = require("./api/trabajitos.router");
+const municipalityRouter = require("./api/municipality.router");
+const statusRouter = require("./api/status.router");
+const categoryRouter = require("./api/category.router");
+const portfolioRouter = require("./api/portfolio.router");
+
+
+router.use("/auth", authRouter);
+router.use("/trabajito", trabajitoRouter);
+router.use("/municipality", municipalityRouter);
+router.use("/status", statusRouter);
+router.use("/category", categoryRouter);
+router.use("/portfolio", portfolioRouter);
 
 module.exports = router;
